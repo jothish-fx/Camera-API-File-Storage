cae0ddd9ebeee340839235bea6e29784680d85fd
Jothish FX <fxjothish@gmail.com>
2022-01-29 11:52:51 +0400

    Initial Commit. Setup Base Project. Added Dependencies. Added Version Catalog. Converted Gradle from Groovy to DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.fx.cameraapifilestorage"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.corePlugins)
+    implementation(libs.bundles.navigation)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.timber)
+    testImplementation(libs.bundles.testPlugins)
+    androidTestImplementation(libs.bundles.androidTestPlugins)
+}
